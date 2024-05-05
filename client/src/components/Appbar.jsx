084c62a0947c3d72a43a299a7a08d711656b5f1c
Adamya Agrawal <adamyabansal@gmail.com>
2024-05-05 22:28:34 +0530

    changed class to classname

--- client/src/components/Appbar.jsx
+++ client/src/components/Appbar.jsx
@@ -4,12 +4,12 @@ export default function Appbar() {
     const navigate = useNavigate()
     return (
         <>
-            <nav class="bg-white-400 border-white-200">
-                <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <div class="hidden w-full md:block md:w-auto">
-                        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+            <nav className="bg-white-400 border-white-200">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <div className="hidden w-full md:block md:w-auto">
+                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
                             <li>
-                                <a class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+                                <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
                                     onClick={() => {
                                         navigate('/')
                                     }}>
@@ -17,7 +17,7 @@ export default function Appbar() {
                                 </a>
                             </li>
                             <li>
-                                <a class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+                                <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
                                     onClick={() => {
                                         navigate('/showTodo')
                                     }}>
@@ -25,7 +25,7 @@ export default function Appbar() {
                                 </a>
                             </li>
                             <li>
-                                <a class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+                                <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
                                     onClick={() => {
                                         navigate('/anotherShowTodo')
                                     }}>
