6341ef2b88ca062f178b580a6a03e9fe8511fede
Adamya Agrawal <adamyabansal@gmail.com>
2024-05-12 04:23:54 +0530

    added curley braces in error page .... my mistake

--- client/src/components/ErrorLogin.jsx
+++ client/src/components/ErrorLogin.jsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom"
 
-export default function ErrorLogin({ ErrorMessage = "Error" }) {
-
+export default function ErrorLogin({ errorMessage = "Error" }) {
     const navigate = useNavigate();
     // error related to API will be redirected to this page
     // use this page if cookies are not set to redirect
@@ -9,9 +8,9 @@ export default function ErrorLogin({ ErrorMessage = "Error" }) {
         <div className="h-screen bg-gray-400 flex flex-col justify-center items-center">
             <div className="bg-white rounded-xl p-5 pt-3">
                 <div className="font-bold text-3xl pb-3">
-                    {{ ErrorMessage }}
+                    {errorMessage}
                 </div>
-                <div className="">
+                <div>
                     Please click on <span onClick={() => { navigate('/signin') }} className="hover:underline underline-offset-2">Signin</span>
                 </div>
             </div>
