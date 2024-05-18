b7e720fad6e6e636a31f254980f1e0ca1d38b7e7
Adamya Agrawal <adamyabansal@gmail.com>
2024-05-18 21:24:20 +0530

    sanity on delete confirmation

--- client/src/components/ShowTodos.jsx
+++ client/src/components/ShowTodos.jsx
@@ -78,6 +78,26 @@ export default function ShowTodos() {
         }
     };
 
+    const DeleteConfirmationModel = () => (
+        <div id="deleteModal" aria-hidden="true" className="flex shadow-2xl overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+            <div className="relative p-4 w-full max-w-md h-full md:h-auto">
+                <div className="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+                    <svg className="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"></path></svg>
+                    <p className="mb-4 text-gray-500 dark:text-gray-300">Are you sure you want to delete this Task?</p>
+                    <div className="flex justify-center items-center space-x-4">
+                        <button onClick={() => deleteConfirmation(-1)} data-modal-toggle="deleteModal" type="button" className="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                            No, cancel
+                        </button>
+                        <button onClick={() => deleteTask(taskToDelete)} type="submit" className="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
+                            Yes, I'm sure
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+
     // useEffect works as, whenever a option changes inside of the passed array ([option1, option2]) then the passed function will be executed.
     // When nothing is passed ([]) then the function will run only once at the time of mount, because nothing changes inside an empty array.
     useEffect(() => {
@@ -89,7 +109,6 @@ export default function ShowTodos() {
 
     return (
         <>
-            <br />
             <br />
             <br />
             <div className="w-1/2 mx-auto relative">
@@ -152,24 +171,7 @@ export default function ShowTodos() {
                     </tbody>
                 </table>
             </div>
-            {showDeleteConfirmation &&
-                (<div id="deleteModal" aria-hidden="true" className="flex shadow-2xl overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
-                    <div className="relative p-4 w-full max-w-md h-full md:h-auto">
-                        <div className="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
-                            <svg className="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"></path></svg>
-                            <p className="mb-4 text-gray-500 dark:text-gray-300">Are you sure you want to delete this Task?</p>
-                            <div className="flex justify-center items-center space-x-4">
-                                <button onClick={() => deleteConfirmation(-1)} data-modal-toggle="deleteModal" type="button" className="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
-                                    No, cancel
-                                </button>
-                                <button onClick={() => deleteTask(taskToDelete)} type="submit" className="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
-                                    Yes, I'm sure
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>)
-            }
+            {showDeleteConfirmation && <DeleteConfirmationModel />}
         </>
     )
 }
