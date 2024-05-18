56d199f4f144b182ff9fd9f14a1d35c576a11170
Adamya Agrawal <adamyabansal@gmail.com>
2024-05-18 23:42:16 +0530

    added pointer in navbar

--- client/src/components/Appbar.jsx
+++ client/src/components/Appbar.jsx
@@ -9,7 +9,7 @@ export default function Appbar() {
                 <div className="max-w-screen-xl flex items-center justify-center mx-auto p-4">
                     <ul className="font-medium py-2 px-5 flex flex-col border border-gray-100 rounded-lg bg-gray-50 shadow-md md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
                         <li className="flex-grow">
-                            <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-nowrap"
+                            <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-nowrap cursor-pointer"
                                 onClick={() => {
                                     navigate('/user/showTodo', { state: { rule: 0 } });
                                     window.location.reload();
@@ -18,7 +18,7 @@ export default function Appbar() {
                             </a>
                         </li>
                         <li className="flex-grow">
-                            <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-500 md:p-0 text-nowrap"
+                            <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-500 md:p-0 text-nowrap cursor-pointer"
                                 onClick={() => {
                                     navigate('/user/createTodo')
                                 }}>
@@ -26,7 +26,7 @@ export default function Appbar() {
                             </a>
                         </li>
                         <li className="flex-grow">
-                            <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-nowrap"
+                            <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-nowrap cursor-pointer"
                                 onClick={() => {
                                     navigate('/user/showTodo', { state: { rule: 1 } });
                                     window.location.reload();
@@ -35,7 +35,7 @@ export default function Appbar() {
                             </a>
                         </li>
                         <li className="flex-grow">
-                            <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-nowrap"
+                            <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-nowrap cursor-pointer"
                                 onClick={() => {
                                     navigate('/user/showTodo', { state: { rule: 2 } });
                                     window.location.reload();
@@ -44,7 +44,7 @@ export default function Appbar() {
                             </a>
                         </li>
                         <li className="flex-grow">
-                            <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-nowrap"
+                            <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-nowrap cursor-pointer"
                                 onClick={() => {
                                     navigate('/user/showTodo', { state: { rule: 3 } });
                                     window.location.reload();
@@ -53,7 +53,7 @@ export default function Appbar() {
                             </a>
                         </li>
                         <li className="flex-grow">
-                            <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-nowrap"
+                            <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-nowrap cursor-pointer"
                                 onClick={() => {
                                     navigate('/user/ShowTodo', { state: { rule: 4 } });
                                     window.location.reload();
@@ -62,7 +62,7 @@ export default function Appbar() {
                             </a>
                         </li>
                         <li className="flex-grow">
-                            <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0 text-nowrap"
+                            <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0 text-nowrap cursor-pointer"
                                 onClick={() => {
                                     navigate('/signin');
                                     removeCookie("token");
